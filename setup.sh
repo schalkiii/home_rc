22b99f66746350a84b9b96486858f2a7bab53d88
schalkiii <schalkiii@cysic.xyz>
2025-05-20 14:04:14 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+cp .gitconfig ~/
+cp .zshrc ~/
+cp .vimrc ~/
+cp -r .vim ~/
+cp -r .oh-my-zsh ~/
+cp -r elisp ~/
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
