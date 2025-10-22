4bfa90f1e11c2b685d0a992ae09f505b79800267
schalkiii <30932387+schalkiii@users.noreply.github.com>
2025-10-22 23:22:04 +0800

    Add Vim plugins for tabular and supertab

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ cp -r .oh-my-zsh ~/
 cp -r elisp ~/
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/godlygeek/tabular ~/.vim/pack/plugins/start/tabular
+git clone https://github.com/ervandew/supertab ~/.vim/pack/plugins/start/supertab
 cp .gitconfig ~/
